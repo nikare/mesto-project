87fa25d052e05e63b8316523905540daa5d30894
Nikolay Karetskiy <nkaretskiy@novakidschool.com>
2023-06-16 20:14:17 +0200

    Complete 2st task

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,36 @@ const closePopupButtons = document.querySelectorAll('.popup__close-button');
 const profileNameEl = document.querySelector('.profile__name');
 const profileDescriptionEl = document.querySelector('.profile__description');
 
+const cardsListEl = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#card');
+
+const cards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
 // profile
 function fillProfilePopup() {
   const { name, desc } = document.forms.profile;
@@ -16,6 +46,30 @@ function fillProfileContent() {
   const { name, desc } = document.forms.profile;
   profileNameEl.textContent = name.value;
   profileDescriptionEl.textContent = desc.value;
+}
+
+// cards
+renderCards();
+
+function createCard(name, link) {
+  const card = cardTemplate.content.cloneNode(true);
+  const image = card.querySelector('.card__image');
+  const title = card.querySelector('.card__title');
+
+  image.src = link;
+  image.alt = name;
+  title.textContent = name;
+
+  return card;
+}
+
+function renderCards() {
+  cardsListEl.innerHTML = '';
+
+  cards.forEach(({ name, link }) => {
+    const card = createCard(name, link);
+    cardsListEl.append(card);
+  });
 }
 
 // events
