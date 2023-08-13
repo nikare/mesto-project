57bba3823d2e99e4ebb3a7c1301c0ffd5df7dea9
Nikolay Karetskiy <nkaretskiy@novakidschool.com>
2023-08-13 17:15:58 +0200

    feature(validation): close popup by escape and overlay

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,7 +1,24 @@
+const popups = Array.from(document.querySelectorAll('.popup'));
+
+popups.forEach((popup) => {
+  popup.addEventListener('click', (event) => {
+    if (event.target !== popup) return;
+    closePopup(popup);
+  });
+});
+
 export function openPopup(popup) {
   popup.classList.add('popup_active');
+  document.addEventListener('keydown', closePopupByEscape);
 }
 
 export function closePopup(popup) {
   popup.classList.remove('popup_active');
+  document.removeEventListener('keydown', closePopupByEscape);
+}
+
+function closePopupByEscape(event) {
+  if (event.key !== 'Escape') return;
+  const activePopup = popups.find((popup) => popup.classList.contains('popup_active'));
+  closePopup(activePopup);
 }
